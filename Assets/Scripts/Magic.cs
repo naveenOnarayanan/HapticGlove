d4f3479cc2b32a4891584edb5b2bf3a49ba7374a
Owen Wang <wangfowen@gmail.com>
2014-06-14 16:14:48 -0400

    added script for creating fire

---
+++ Assets/Scripts/Magic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//instantiate fire object
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if press key, create fire object in front of view
+		if (Input.GetKeyDown ("f")) {
+			GameObject fire = (GameObject)Instantiate(Resources.Load ("Flame"));
+			Vector3 vector = new Vector3(transform.position.x, 
+			                             0, 
+			                             transform.position.z);
+			fire.transform.position += vector;
+		}
+	}
+}
