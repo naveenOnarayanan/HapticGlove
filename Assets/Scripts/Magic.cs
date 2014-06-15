4cbd99cc5341089b5673ae580689c25099ac1001
Owen Wang <wangfowen@gmail.com>
2014-06-14 22:55:27 -0400

    centered fire and added ice

--- Assets/Scripts/Magic.cs
+++ Assets/Scripts/Magic.cs
@@ -1,8 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Magic : MonoBehaviour {
+	enum MagicType {
+		Fire,
+		Ice
+	};
 
+	void CreateObject(MagicType type) {
+		GameObject obj;
+
+		switch (type) {
+			case MagicType.Fire:
+				obj = (GameObject)Instantiate(Resources.Load ("Flame"));
+				break;
+			default:
+				obj = (GameObject)Instantiate(Resources.Load ("Stream"));
+				break;
+		}
+
+		Vector3 vector = Camera.main.ViewportToWorldPoint (new Vector3(0.5f, 0, 2f));
+		obj.transform.position += vector;
+	}
+	
 	// Use this for initialization
 	void Start () {
 		//instantiate fire object
@@ -10,13 +30,14 @@ public class Magic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//if press key, create fire object in front of view
 		if (Input.GetKeyDown ("f")) {
-			GameObject fire = (GameObject)Instantiate(Resources.Load ("Flame"));
-			Vector3 vector = new Vector3(transform.position.x, 
-			                             0, 
-			                             transform.position.z);
-			fire.transform.position += vector;
+			CreateObject (MagicType.Fire);
+			//TODO: send signal for heat to server
+		}
+
+		if (Input.GetKeyDown ("g")) {
+			CreateObject (MagicType.Ice);
+			//TODO: send signal for cold to server
 		}
 	}
 }
