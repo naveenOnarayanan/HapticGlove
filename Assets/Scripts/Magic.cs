72f92e78f7850f40d85994aeeac2759e67cc724f
Owen Wang <wangfowen@gmail.com>
2014-07-28 11:47:49 -0400

    hooked up to server

--- Assets/Scripts/Magic.cs
+++ Assets/Scripts/Magic.cs
@@ -1,132 +1,175 @@
-using UnityEngine;
-using System.Collections;
 using Leap;
+using SimpleJSON;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
 
 public class Magic : MonoBehaviour {
-	public WWW server;
-	Controller controller;
-	Hand mainHand;
+    public Socket client;
+    IPEndPoint servoServer;
+    IPEndPoint peltierServer;
+    
+    public int servoPort;
+    public int peltierPort;
+    public string IP;
+  	
+    Controller controller;
+  	Hand mainHand;
 
-	int counter = 0;
+  	int counter = 0;
 
-	GameObject obj;
-	MagicType lastCall;
-  bool fireballCharged = false;
+  	GameObject obj;
+  	MagicType lastCall;
+    bool fireballCharged = false;
+    bool cooldown = false;
 
+  	enum MagicType {
+  		FireSmall,
+  		FireMedium,
+  		FireLarge,
+  		Fireball,
+  		Ice
+  	};
 
-	enum MagicType {
-		FireSmall,
-		FireMedium,
-		FireLarge,
-		Fireball,
-		Ice
-	};
+  	void CreateObject(MagicType type) {
+    		Vector3 vector = transform.TransformPoint (mainHand.PalmPosition.ToUnityScaled ());
+    		if (obj != null && type == lastCall) {
+      			obj.transform.position = transform.TransformPoint (mainHand.PalmPosition.ToUnityScaled ());
+      			obj.transform.rotation = transform.rotation;
+    		} else {
+            if (lastCall == MagicType.Fireball) {
+                Destroy (obj, 5);
+            } else {
+                Destroy(obj);
+            }
+    				switch (type) {
+      					case MagicType.FireSmall:
+                    obj = (GameObject)Instantiate (Resources.Load ("Fire_01"), vector, transform.rotation);
+        						break;
+      					case MagicType.FireMedium:
+                    obj = (GameObject)Instantiate (Resources.Load ("Fire_02"), vector, transform.rotation);
+        						break;
+                case MagicType.FireLarge:
+                    obj = (GameObject) Instantiate (Resources.Load ("Fire_03"), vector, transform.rotation);
+                    break;
+      					case MagicType.Fireball:
+                    // TODO: Need to get the fireball to be shot where the user is facing
+                    Debug.Log (Camera.main.transform.rotation);
+                    obj = (GameObject)Instantiate(Resources.Load ("Fireball"), vector, Camera.main.transform.rotation);	
+        						break;
+      					default:
+        						obj = (GameObject)Instantiate (Resources.Load ("Stream"));
+        						break;
+    				}
+    		}
 
-	void CreateObject(MagicType type) {
-		Vector3 vector = transform.TransformPoint (mainHand.PalmPosition.ToUnityScaled ());
-		if (obj != null && type == lastCall) {
-			obj.transform.position = transform.TransformPoint (mainHand.PalmPosition.ToUnityScaled ());
-			obj.transform.rotation = transform.rotation;
-		} else {
-        if (lastCall == MagicType.Fireball) {
-          Destroy (obj, 5);
-        } else {
-          Destroy(obj);
-        }
-				switch (type) {
-					case MagicType.FireSmall:
-            obj = (GameObject)Instantiate (Resources.Load ("Fire_01"), vector, transform.rotation);
-						break;
-					case MagicType.FireMedium:
-            obj = (GameObject)Instantiate (Resources.Load ("Fire_02"), vector, transform.rotation);
-						break;
-          case MagicType.FireLarge:
-            obj = (GameObject) Instantiate (Resources.Load ("Fire_03"), vector, transform.rotation);
+  		  lastCall = type;
+  	}
+  	
+    IEnumerator Delay(int seconds) {
+        yield return new WaitForSeconds (seconds);
+    }
+
+    void sendData(string data, IPEndPoint server) {
+        //TODO: process data as json
+        Int32 timestamp = (Int32)(DateTime.UtcNow.Subtract (new DateTime (1970, 1, 1))).TotalSeconds;
+        string json = "{\"timestamp\":" + timestamp + ", " + data + "}";
+        byte[] d = Encoding.UTF8.GetBytes (json);
+
+        client.SendTo (d, d.Length, SocketFlags.None, server);
+    }
+    
+  	// Use this for initialization
+  	void Start () {
+    		controller = new Controller ();
+    		controller.EnableGesture (Gesture.GestureType.TYPE_CIRCLE);
+    		controller.EnableGesture (Gesture.GestureType.TYPE_SWIPE);
+        
+    		//instantiate server
+        IP = "169.254.191.81";
+        servoPort = 3000;
+        peltierPort = 3001;
+        servoServer = new IPEndPoint (IPAddress.Parse (IP), servoPort);
+        peltierServer = new IPEndPoint (IPAddress.Parse (IP), peltierPort);
+        client = new Socket (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+  	}
+  	
+  	// Update is called once per frame
+  	void Update () {
+  		  Frame frame = controller.Frame ();
+    		mainHand = controller.Frame().Hands[0];
+
+    		//Debug.Log (mainHand.Direction + ":" + mainHand.PalmNormal);
+
+    		Pointable finger;
+    		bool fingerFlat = true;
+
+    		//Debug.Log (transform.TransformPoint(mainHand.PalmPosition.ToUnityScaled()));
+
+    		for (int i = 0; i < controller.Frame().Pointables.Count; i++) {
+      			finger = controller.Frame ().Pointables[i];
+      			fingerFlat &= finger.Direction.y > 0;
+    		}
+        
+        
+        Gesture.GestureType gesture = Gesture.GestureType.TYPE_INVALID;
+
+        foreach (Gesture gestures in frame.Gestures ()) {
+            gesture = gestures.Type;
             break;
-					case MagicType.Fireball:
-            // TODO: Need to get the fireball to be shot where the user is facing
-            Debug.Log (Camera.main.transform.rotation);
-            obj = (GameObject)Instantiate(Resources.Load ("Fireball"), vector, Camera.main.transform.rotation);	
-						break;
-					default:
-						obj = (GameObject)Instantiate (Resources.Load ("Stream"));
-						break;
-				}
-		}
+        }
+        
+        // Metrics to detect if hand is face-up with fingers open (indicates if hand can charge fireball)
+    		if (mainHand.Direction.y > 0.5 && mainHand.PalmNormal.y < 0 && mainHand.PalmNormal.z < 0 && fingerFlat) {
+            //Debug.Log (counter);
+            if (counter <= 600 && !cooldown) {
+                // Counts for 200 frames before switching to larger flame
+                if (counter > 200 && counter <= 400) {
+                    CreateObject(MagicType.FireMedium);
+                } else if (counter > 400) {
+                    CreateObject(MagicType.FireLarge);
+                } else {
+                    CreateObject (MagicType.FireSmall);
+                }
 
-		lastCall = type;
-	}
-	
-  IEnumerator Delay(int seconds) {
-    yield return new WaitForSeconds (seconds);
-  }
-  
-  
-	// Use this for initialization
-	IEnumerator Start () {
-		controller = new Controller ();
-		controller.EnableGesture (Gesture.GestureType.TYPE_CIRCLE);
-		controller.EnableGesture (Gesture.GestureType.TYPE_SWIPE);
-    
-    
-		//instantiate server
-		server = new WWW ("http://ip.jsontest.com/");
-		yield return server;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Frame frame = controller.Frame ();
+                fireballCharged = true;
+                //TODO: differentiate hotness when we have that established
+                sendData("\"temperature\": 10", peltierServer);
+                counter++;
+            } else if (fireballCharged) {
+                //TODO: not working as expected. Delay not waiting, and still spamming fire
+                Debug.Log ("Charged too long, in cooldown mode");
+                Destroy (obj.gameObject); 
+                fireballCharged = false;
+                cooldown = true;
 
+                sendData("\"temperature\": 0", peltierServer);
+                sendData ("\"angle\": 180", servoServer);
+                Delay (5);
+                sendData ("\"angle\": 0", servoServer);
+                Debug.Log ("Done cooldown");
+                cooldown = false;
+            }
+        } else if (gesture == Gesture.GestureType.TYPE_CIRCLE && fireballCharged) {
+            // TODO: need to add logic such that if the fireball was not shot within X amount of time then
+            // fireball will not be released
+            CreateObject (MagicType.Fireball);
+            fireballCharged = false;
+            sendData("\"temperature\": 0", peltierServer);
+        } else {
+            if (lastCall == MagicType.Fireball) {
+                Debug.Log ("Destroying since it is not fireball");
+                Delay (10);
+            }
 
-		mainHand = controller.Frame().Hands[0];
-
-		//Debug.Log (mainHand.Direction + ":" + mainHand.PalmNormal);
-
-		Pointable finger;
-		bool fingerFlat = true;
-
-		//Debug.Log (transform.TransformPoint(mainHand.PalmPosition.ToUnityScaled()));
-
-		for (int i = 0; i < controller.Frame().Pointables.Count; i++) {
-			finger = controller.Frame ().Pointables[i];
-			fingerFlat &= finger.Direction.y > 0;
-		}
-    
-    
-    Gesture.GestureType gesture = Gesture.GestureType.TYPE_INVALID;
-    foreach (Gesture gestures in frame.Gestures ()) {
-      gesture = gestures.Type;
-      break;
-    }
-    
-    // Metrics to detect if hand is face-up with fingers open (indicates if hand can charge fireball)
-		if (mainHand.Direction.y > 0.5 && mainHand.PalmNormal.y < 0 && mainHand.PalmNormal.z < 0 && fingerFlat) {
-      Debug.Log (counter);
-      // Counts for 200 frames before switching to larger flame
-      if (counter > 200 && counter <= 500) {
-        CreateObject(MagicType.FireMedium);
-      } else if (counter > 500) {
-        CreateObject(MagicType.FireLarge);
-      } else {
-        CreateObject (MagicType.FireSmall);
-      }
-      fireballCharged = true;
-      counter++;
-    } else if (gesture == Gesture.GestureType.TYPE_CIRCLE && fireballCharged) {
-      // TODO: need to add logic such that if the fireball was not shot within X amount of time then
-      // fireball will not be released
-      CreateObject (MagicType.Fireball);
-      fireballCharged = false;
-    } else {
-      if (lastCall == MagicType.Fireball) {
-        Debug.Log ("Destroying since it is not fireball");
-        Delay (10);
-      }
-      if (obj != null && lastCall != MagicType.Fireball) {
-        Destroy (obj.gameObject); 
-      }
-      counter = 0;
-    }
-	}
+            if (obj != null && lastCall != MagicType.Fireball) {
+                Destroy (obj.gameObject); 
+            }
+            counter = 0;
+        }
+  	}
 }
--- client.py
+++ client.py
@@ -4,29 +4,33 @@ from time import sleep
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('169.254.191.81', 3000)
-server_address_2 = ('169.254.191.81', 3001)
+servo_server = ('169.254.191.81', 3000)
+peltier_server = ('169.254.191.81', 3001)
 
 try:
     message = '{"timestamp": 1404330711, "angle": 180}'
     print>>sys.stderr, 'sending %s' % message
-    sock.sendto(message, server_address)
-    sleep(2)
+    sock.sendto(message, servo_server)
+    sleep(5)
+    message = '{"timestamp": 1404330711, "angle": 5}'
+    print>>sys.stderr, 'sending %s' % message
+    sock.sendto(message, servo_server)
+    sleep(5)
     message = '{"timestamp": 1404330711, "angle": 0}'
     print>>sys.stderr, 'sending %s' % message
-    sock.sendto(message, server_address)
-    sleep(2)
+    sock.sendto(message, servo_server)
+    sleep(5)
     message = '{"timestamp": 1404330711, "temperature": 10}'
     print>>sys.stderr, 'sending %s' % message
-    sock.sendto(message, server_address_2)
-    sleep(2)
+    sock.sendto(message, peltier_server)
+    sleep(5)
     message = '{"timestamp": 1404330711, "temperature": -10}'
     print>>sys.stderr, 'sending %s' % message
-    sock.sendto(message, server_address_2)
-    sleep(2)
+    sock.sendto(message, peltier_server)
+    sleep(5)
     message = '{"timestamp": 1404330711, "temperature": 0}'
     print>>sys.stderr, 'sending %s' % message
-    sock.sendto(message, server_address_2)
+    sock.sendto(message, peltier_server)
 
 finally:
     print >> sys.stderr, 'closing socket'
