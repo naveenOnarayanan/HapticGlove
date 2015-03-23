78402384a559b644ab85925b4af1dac96b991a02
Owen Wang <wangfowen@gmail.com>
2015-03-23 17:57:26 -0400

    explanations code cleaner

--- Assets/Scripts/ExplanationControl.cs
+++ Assets/Scripts/ExplanationControl.cs
@@ -3,30 +3,13 @@ using System.Collections.Generic;
 
 
 public class ExplanationControl : MonoBehaviour {
-
-    const float EXPLANATION_TIME = 5f;
+	
     int counter = 0;
-    float deltaTime = 5f;
+    float deltaTime = 0f;
     bool continueInstructions = true;
 	private NetworkController nc;
-
-    class Explanation {
-        public string explanationTxtL1 = "";
-        public string explanationTxtL2 = "";
-        public Vector3 positionL1;
-        public Vector3 positionL2 = new Vector3(0, 0);
-        public Explanation(string explanationTxtL1, Vector3 positionL1, string explanationTxtL2, Vector3 positionL2) {
-            this.explanationTxtL1 = explanationTxtL1;
-            if (explanationTxtL2 != null) {
-              this.explanationTxtL2 = explanationTxtL2;
-            }
-            this.positionL1 = positionL1;
-            if (positionL2 != null) {
-              this.positionL2 = (Vector3)positionL2;
-            }
-        }
-    }
-
+	TextMesh explanationText;
+	
     List<Explanation> explanations = new List<Explanation>();
 
 	void OnApplicationQuit() {
@@ -35,111 +18,67 @@ public class ExplanationControl : MonoBehaviour {
 		}
 	}
 
+	abstract class Explanation {
+		public string text = "";
+
+		public abstract bool ReadyForNextInstr (float deltaTime);
+	}
+
+	class TimedExplanation : Explanation {
+		float endTime = 0f;
+
+		public TimedExplanation(string text, float endTime) {
+			this.text = text;
+			this.endTime = endTime;
+		}
+
+		public override bool ReadyForNextInstr(float deltaTime) {
+			if (deltaTime >= endTime) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
   	void Start () {
         GameObject explanationTextL1 = GameObject.FindGameObjectWithTag ("ExplanationText_L1");
-        GameObject explanationTextL2 = GameObject.FindGameObjectWithTag ("ExplanationText_L2");
+		explanationText = explanationTextL1.GetComponent<TextMesh> ();
+
+		//if you charge too long, you'll overload and be unable to cast spells for a while
+		//defend yourself with ice walls
+		//hold your palm forward towards the screen <PALM FORWARD IMAGE>
+		//now make a circular motion <CIRCLE IMAGE>
+		//keep holding your palm out to charge it. the more charged the stronger the wall
+		//you are now ready to fight!
 
         // Adding initial configuration message
-        explanations.Add(new Explanation(
-            "Lets configure", new Vector3(-5.866141f, 7.897055f),
-            "your new glove!", new Vector3(-6.437281f, 4.92359f)
-        ));
-        explanations.Add (new Explanation("Please make a fist!", new Vector3(-8.111927f, 7.41565f), null, new Vector3(0, 0)));
-		explanations.Add (new Explanation("Good! Now relax", new Vector3(-6.425037f, 7.897055f), "your hand!", new Vector3(-6.437281f, 4.92359f)));
-		explanations.Add (new Explanation("Face your palm", new Vector3(-6.425037f, 7.897055f), "upwards", new Vector3(-6.437281f, 4.92359f)));
-		explanations.Add (new Explanation("Now, face your", new Vector3(-6.425037f, 7.897055f), "palm forwards", new Vector3(-6.437281f, 4.92359f)));
+		explanations.Add(new TimedExplanation("Prepare for battle! You are about to\npartake in a magical duel...\nTO THE DEATH!!", 3));
+        explanations.Add(new TimedExplanation("Arm yourself with fireballs\nTo create a fireball, first make a fist.", 3));
+		explanations.Add(new TimedExplanation("Now open your fist, palm facing up.", 3));
+		explanations.Add(new TimedExplanation("This charges the fire. The more charged,\nthe more damage it does.", 3));
+		explanations.Add(new TimedExplanation("Thrust your palm forwards to shoot it.", 3));
+
+		explanationText.text = explanations[counter].text;
+
 		nc = NetworkController.instance();
 		//nc.accelGyro();
     }
 
-	bool ReadyForThisInstr() {
-		return deltaTime < 0;
-	}
-
-	bool EndOfThisInstr() {
-		return deltaTime == 1;
-	}
-
   	// Update is called once per frame
   	void Update () {
-		deltaTime -= Time.deltaTime;
+		deltaTime += Time.deltaTime;
 
 		//load next explanation
-		if (ReadyForThisInstr() && continueInstructions) {
-			if (counter >= 5) {
+		if (explanations[counter].ReadyForNextInstr(deltaTime)) {
+			counter = counter + 1;
+
+			if (counter == explanations.Count) {
 				Application.LoadLevel(Application.loadedLevel + 1);
-				return;
-			}
-            // Find two explanation text lines
-            GameObject explanationTextL1 = GameObject.FindGameObjectWithTag ("ExplanationText_L1");
-            GameObject explanationTextL2 = GameObject.FindGameObjectWithTag ("ExplanationText_L2");
-
-            Vector3 textPositionL1 = explanations[counter].positionL1;
-            Vector3 textPositionL2 = explanations[counter].positionL2;
-            textPositionL1.z = textPositionL2.z = explanationTextL1.transform.position.z;
-
-            explanationTextL1.GetComponent<TextMesh>().text = explanations[counter].explanationTxtL1;
-            explanationTextL2.GetComponent<TextMesh>().text = explanations[counter].explanationTxtL2;
-
-            explanationTextL1.transform.position = textPositionL1;
-            explanationTextL2.transform.position = textPositionL2;
-
-            deltaTime = EXPLANATION_TIME;
-            counter = counter + 1;
-
-			// Prevents text from changing until we receive calibrated values from the game
-			if (counter > 1) {
-				continueInstructions = false;
+			} else {
+				explanationText.text = explanations[counter].text;
+				deltaTime = 0;
 			}
         }
-
-		/*
-		// Reset bottom motor for closed fist
-		if (counter == 2) {
-			if (ReadyForThisInstr()) {
-				nc.resetBottomServo();
-				deltaTime = 3f;
-				continueInstructions = true;
-			}
-
-			if (EndOfThisInstr()) {
-				UserData.servo_closed_fist = UserData.servo;
-			}
-		}
-		// Reset top 2 motors for Open palm
-		else if (counter == 3) {
-			if (ReadyForThisInstr()) {
-				nc.resetTopServo();
-				deltaTime = 3f;
-				continueInstructions = true;
-			}
-
-			if (EndOfThisInstr()) {
-				UserData.servo_neutral = UserData.servo;
-			}
-		}
-		// Open palm facing up
-		else if (counter == 4) {
-			if (ReadyForThisInstr()) {
-				deltaTime = 3f;
-				continueInstructions = true;
-			}
-
-			if (EndOfThisInstr()) {
-				UserData.accel_gyro_face_up = UserData.accel_gyro;
-			}
-		}
-		// Open palm facing forward
-		else if (counter == 5) {
-			if (ReadyForThisInstr()) {
-				deltaTime = 3f;
-				continueInstructions = true;
-			}
-
-			if (EndOfThisInstr()) {
-				UserData.accel_gyro_face_forward = UserData.accel_gyro;
-			}
-		}
-		*/
   	}
 }
